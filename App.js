4ac5b6f5e4e9b577e430ebfd8d9ad5ec42c40303
Luana Stephany <ls.luanastephany@gmail.com>
2021-12-30 15:45:37 -0300

    creating app

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [url, setUrl] = useState('')
+  const [name, setName] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>url
+        <Text style={{color: '#1076f7'}}>Sujeito</Text>
+      </Text>
+
+      <TextInput 
+        style={styles.urlInput} 
+        onChangeText={(texto) => setUrl(texto)} 
+        value={url} 
+        placeholder="Digite a url" 
+      />
+
+      <TextInput 
+        style={styles.urlInput} 
+        onChangeText={(texto) => setName(texto)} 
+        value={name} 
+        placeholder="Nome personalizado" 
+      />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  title: {
+    color: '#21243d',
+    fontWeight: 'bold',
+    fontSize: 40,
+    marginBottom: 20,
+  },
+  urlInput: {
+    height: 50,
+    width: '80%',
+    borderColor: '#21243d',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: '#fafafa',
+    marginBottom: 20,
+    fontSize: 20,
+
+  }
+})
